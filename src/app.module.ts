42cd2c43d2c03ea8e9994408acf6d5c8a1719f4d
omargamalryze <o.gamal@ryze.website>
2025-02-05 11:49:47 +0200

    add Sequelize Module connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      models: [],
+    }),
+  ],
   providers: [],
 })
 export class AppModule {}
